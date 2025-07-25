d377e013874f5d75259d7e6fc8ad8ee7a25e7884
jonasluis <jonasluis66@gmail.com>
2025-07-25 10:41:20 -0300

    feat:  adicionar a tratamento de erros e prioridade do filtro de acesso

--- src/main/java/med/voll/api/infra/security/TokenService.java
+++ src/main/java/med/voll/api/infra/security/TokenService.java
@@ -24,7 +24,6 @@ public class TokenService {
             return JWT.create()
                     .withIssuer("API Voll.med")
                     .withSubject(usuario.getLogin())
-                    .withClaim("id", usuario.getId())
                     .withExpiresAt(dataExpiricao())
                     .sign(algoritimo);
         } catch (JWTCreationException exception){
